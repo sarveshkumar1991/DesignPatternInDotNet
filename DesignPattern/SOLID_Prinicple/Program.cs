b5790dd7a27a76ca5d56e75a4d1350a5f57849cc
sarvesh_kumar1991@yahoo.in <sarvesh_kumar1991@yahoo.in>
2019-03-18 12:31:55 +0530

    initial

---
+++ DesignPattern/SOLID_Prinicple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLID_Prinicple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
