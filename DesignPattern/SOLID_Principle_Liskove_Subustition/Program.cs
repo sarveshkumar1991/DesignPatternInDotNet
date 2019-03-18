637b74e24beabe13a39abd8aee9a9189b6ed2218
sarvesh_kumar1991@yahoo.in <sarvesh_kumar1991@yahoo.in>
2019-03-18 12:49:15 +0530

    Solid principle

---
+++ DesignPattern/SOLID_Principle_Liskove_Subustition/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Dynamic;
+namespace SOLID_LiskovSubstitutionPrinciple
+{
+
+
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $" width:{this.Width} and height:{this.Height}";
+        }
+
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set
+            {
+                base.Height = base.Width = value;
+            }
+        }
+
+        public override int Height
+        {
+            set
+            {
+                base.Height = base.Width = value;
+            }
+        }
+
+
+    }
+
+
+
+
+    public class LiskovSubProgram
+    {
+
+        public static int Area(Rectangle rectangle) => rectangle.Height * rectangle.Width;
+
+
+        static void Main(string[] args)
+        {
+
+            Rectangle rectangle = new Rectangle(3, 6);
+            int rectangleArea = Area(rectangle);
+            Console.WriteLine($"Area of {nameof(rectangle)} is {rectangleArea} of {rectangle.ToString()}");
+
+
+            Rectangle square = new Square();
+            square.Width = 4;
+            int squareArea = Area(square);
+            Console.WriteLine($"Area of {nameof(square)} is {squareArea} of {square.ToString()}");
+
+
+
+        }
+    }
+}
--- DesignPattern/SOLID_Prinicple/Program.cs
+++ DesignPattern/SOLID_Prinicple/Program.cs
@@ -1,12 +1,236 @@
 ﻿using System;
-
-namespace SOLID_Prinicple
+using System.Collections.Generic;
+using System.Collections;
+using System.Net.Http.Headers;
+namespace SOLID_OpenClosePrinicple
 {
+
+    public enum Size
+    {
+        VerySmall,
+        Small,
+        Medium,
+        Large
+    }
+
+    public enum Color
+    {
+        Red,
+        Blue,
+        Green
+    }
+
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+
+        public Product(string name, Color color, Size size)
+        {
+            this.Name = name;
+            this.Size = size;
+            this.Color = color;
+        }
+
+    }
+
+
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfy(T t);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color _color;
+        public ColorSpecification(Color color)
+        {
+            this._color = color;
+        }
+
+        public bool IsSatisfy(Product t)
+        {
+            if (t.Color == _color)
+                return true;
+            else
+                return false;
+        }
+    }
+
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size _Size;
+        public SizeSpecification(Size size)
+        {
+            this._Size = size;
+        }
+
+        public bool IsSatisfy(Product t)
+        {
+            if (t.Size == _Size)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class SizeAndColorSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first;
+        private ISpecification<T> second;
+        public SizeAndColorSpecification(ISpecification<T> spec1, ISpecification<T> spec2)
+        {
+            this.first = spec1;
+            this.second = spec2;
+        }
+
+        public bool IsSatisfy(T t)
+        {
+            if (this.first.IsSatisfy(t) && this.second.IsSatisfy(t))
+                return true;
+            else
+                return false;
+        }
+    }
+
+
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> FilterProduct(IEnumerable<T> products, ISpecification<T> specification);
+    }
+
+    public class ProductFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> FilterProduct(IEnumerable<Product> products, ISpecification<Product> specification)
+        {
+            foreach (var item in products)
+            {
+                if (specification.IsSatisfy(item))
+                    yield return item;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            var tree = new Product("Tree", Color.Blue, Size.Large);
+            var apple = new Product("apple", Color.Green, Size.Small);
+            var house = new Product("House", Color.Green, Size.Large);
+            Product[] products = { tree, apple, house };
+
+
+
+            //ProductFilter productFilter = new ProductFilter();
+            //var filterProduct = productFilter.FilterProduct(products, new SizeSpecification(Size.Large));
+            //foreach (var item in filterProduct)
+            //{
+            //    Console.WriteLine($"Product Name:{item.Name}  ----  Size:{item.Size}  ----  Color:{item.Color}  ");
+
+            //}
+
+
+            ProductFilter productFilter = new ProductFilter();
+            var filterProduct = productFilter.FilterProduct(products, new SizeAndColorSpecification<Product>(
+                       new ColorSpecification(Color.Green),
+                  new SizeSpecification(Size.Large)));
+
+
+            foreach (var item in filterProduct)
+            {
+                Console.WriteLine($"Product Name:{item.Name}  ----  Size:{item.Size}  ----  Color:{item.Color}  ");
+
+            }
+
+
+
+
+            //ProductFilter productFilter = new ProductFilter();
+            //var filteredProduct = productFilter.FilterBySize(products, Size.Large);
+            //foreach (var item in filteredProduct)
+            //{
+            //    Console.WriteLine($"Product Name:{item.Name}  ----  Size:{item.Size}  ----  Color:{item.Color}  ");
+            //}
+
+            Console.Read();
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+    ///// <summary>
+    /////  Filter product by color or size
+    ///// </summary>
+
+    //public class ProductFilter
+    //{
+    //    public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+    //    {
+    //        foreach (var item in products)
+    //        {
+    //            if (item.Color == color)
+    //            {
+    //                yield return item;
+    //            }
+    //        }
+    //    }
+
+    //    public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+    //    {
+    //        foreach (var item in products)
+    //        {
+    //            if (item.Size == size)
+    //            {
+    //                yield return item;
+    //            }
+    //        }
+    //    }
+
+    //    public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size, Color color)
+    //    {
+    //        foreach (var item in products)
+    //        {
+    //            if (item.Size == size && item.Color == color)
+    //            {
+    //                yield return item;
+    //            }
+    //        }
+    //    }
+
+    //}
+
+
+
 }
+
