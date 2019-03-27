c4ce1b1a0ab52bbc9d9b96e8e253326f6b24072b
sarvesh_kumar1991@yahoo.in <sarvesh_kumar1991@yahoo.in>
2019-03-27 09:57:05 +0530

    Singleton pattern

---
+++ DesignPattern/SingletonPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+
+    public sealed class Employee
+    {
+        public static int count = 0;
+        public static Employee employee = null;
+
+        private Employee()
+        {
+            count += 1;
+            Console.WriteLine($"Instance count:{count}");
+        }
+
+        public static Employee GetInstance()
+        {
+            if (employee == null)
+            {
+                employee = new Employee();
+            }
+            return employee;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee a = Employee.GetInstance();
+            Employee b = Employee.GetInstance();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
